2955c8b6749d9f514408160de45d31accd3c582b
lisa <lisa@odinsoftware.co.kr>
2019-03-28 12:30:18 +0900

    first commit

---
+++ src/kakao.net/util/KakaoStrapInterop.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Kakao.Net.util
+{
+    public class KakaoStrapInterop
+    {
+        private IJSRuntime JSRuntime { get; set; }
+
+        public KakaoStrapInterop(IJSRuntime jsRuntime)
+        {
+            JSRuntime = jsRuntime;
+        }
+
+        public Task<bool> DrawMap(float latitude, float longitude)
+            => JSRuntime.InvokeAsync<bool>("kakaoMapInterop.drawMap", latitude, longitude);
+
+        public Task<bool> DrawTraffic(float latitude, float longitude)
+            => JSRuntime.InvokeAsync<bool>("kakaoMapInterop.drawTraffic", latitude, longitude);
+    }
+}
